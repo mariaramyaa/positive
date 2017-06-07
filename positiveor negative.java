7a0697fbe66111543da3277cb6743efa766e754a
mariaramyaa <mariaramya96@gmail.com>
2017-06-07 11:05:54 +0530

    Add files via upload

---
+++ positiveor negative.java
@@ -0,0 +1,16 @@
+public class Positive or Negative{
+public void check(int number)
+{
+if(number<0)
+{
+System.out.println(Number+"is a Negative numer");
+}
+else if(number>0)
+{
+System.out.println(numer+"is a positive number");
+}
+else if{
+System.out.println(number+"s a number either positive and negative");
+}
+}
+}
