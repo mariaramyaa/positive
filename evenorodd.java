38de10495dad24fc889c71ae269e94c2a48741c9
mariaramyaa <mariaramya96@gmail.com>
2017-06-07 11:20:11 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,11 @@
+public class even or odd{
+public void check(int number)
+{
+if(number%2==0){
+System.out.println(number+"is the number is even");
+}
+else if(number%2!=0){
+System.out.println(number+"is the number is odd");
+}
+}
+}
