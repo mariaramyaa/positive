62ef8a0a1753a88557cd928f03cbf028a1108608
mariaramyaa <mariaramya96@gmail.com>
2017-07-07 10:27:59 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,37 @@
+package ramya;
+import java.io.*;
+public class Leapyear {
+public static void main(String args[]) {
+	int year = 2000;
+	 
+    //Flag to store the test result
+    boolean isLeapYear = false;
+
+    if(year % 400 == 0)
+    {
+        isLeapYear = true;
+    }
+    else if (year % 100 == 0)
+    {
+        isLeapYear = false;
+    }
+    else if(year % 4 == 0)
+    {
+        isLeapYear = true;
+    }
+    else
+    {
+        isLeapYear = false;
+    }
+
+    //Output the test result
+    if(isLeapYear)
+    {
+        System.out.println("Year "+year+" is a Leap Year");
+    }
+    else
+    {
+        System.out.println("Year "+year+" is not a Leap Year");
+    }
+}
+}
